9363ee92fe7d1cbb2f3062f65555a45771c40719
malicose <mrwesternfan@gmail.com>
2018-10-09 20:36:17 +0300

    first task is done

--- src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
+++ src/main/java/ru/ifmo/cet/javabasics/BottleSong.java
@@ -32,12 +32,61 @@ package ru.ifmo.cet.javabasics;
  */
 public class BottleSong {
 
+    private int atOnes;
+    private int bottles = 99;
+    private static final String[] units = {
+            "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+            "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+    private static final String[] tens = {
+            "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
     public BottleSong(int bottleTakenAtOnce) {
+        this.atOnes = bottleTakenAtOnce;
         //TODO
     }
 
     public String getBottleSongLyrics() {
         //TODO
-        throw new UnsupportedOperationException();
+        //throw new UnsupportedOperationException();
+
+        String s;
+        String s1;
+        if(atOnes < 1 || atOnes > 99) throw new IllegalArgumentException();
+        StringBuffer buf = new StringBuffer();
+        String count = "";
+        if (atOnes < 20){
+            count = units[atOnes];
+        } else if ((atOnes > 19) && (atOnes < 100)){
+            count = tens[atOnes/10 - 2] + ((atOnes%10 != 0) ? " " + units[atOnes%10] : "");
+        }
+
+
+        while (bottles > 0){
+            if (atOnes < bottles){
+                s1 = bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer on the wall, " + bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer.\n" + "Take " + count + " down and pass around, " + (bottles - atOnes) + ((bottles - atOnes !=1)? " bottles" : " bottle") + " of beer on the wall.\n"  ;
+                buf.append(s1);
+                bottles = bottles - atOnes;
+
+            } else if (atOnes == bottles){
+                s1 = bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer on the wall, " + bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer.\n" + "Take " + count + " down and pass around, no more bottles of beer on the wall.\n" +
+                        "No more bottles of beer on the wall, no more bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+                buf.append(s1);
+                bottles = bottles - atOnes;
+            } else{
+                if (bottles < 20){
+                    count = units[bottles];
+                } else if ((bottles > 19) && (bottles < 100)){
+                    count = tens[bottles/10 - 2] + ((bottles%10 != 0) ? " " + units[bottles%10] : "");
+                }
+                s1 = bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer on the wall, " + bottles + ((bottles!=1)? " bottles" : " bottle") + " of beer.\n" + "Take " + count + " down and pass around, no more bottles of beer on the wall.\n" +
+                        "No more bottles of beer on the wall, no more bottles of beer.\n" + "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+                buf.append(s1);
+                bottles = 0;
+            }
+        }
+        s = buf.toString();
+        return s;
+
     }
 }
